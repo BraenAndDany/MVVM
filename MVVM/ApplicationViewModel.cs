3fdc5dc082ceda845cf8fd51dc25d9c0e8ba619f
BraenAndDany <braenanddany@mail.ru>
2023-12-01 09:34:36 +0700

    Добавьте файлы проекта.

---
+++ MVVM/ApplicationViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System.Security.Policy;
+
+namespace MVVM
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Phone selectedPhone;
+
+        public ObservableCollection<Phone> Phones { get; set; }
+        public Phone SelectedPhone
+        {
+            get { return selectedPhone; }
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone { Title="Браен", Company="Бэккэй", Price=1 },
+                new Phone {Title="Стас", Company="Петухов", Price =2 },
+                new Phone {Title="Данила", Company="Ненашев", Price=3 }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
